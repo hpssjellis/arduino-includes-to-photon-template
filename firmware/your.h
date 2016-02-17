50feb9526cd2a0bdeca9c54feb3da8a8ffac15a4
Jeremy Ellis <keyfreemusic@gmail.com>
2016-02-16 22:49:48 -0800

    Update your.h

--- firmware/your.h
+++ firmware/your.h
@@ -1,1 +1,17 @@
-// Your .h file
+// Your.h file
+
+#ifndef LED13_H
+#define LED13_H
+
+#include "WProgram.h"
+
+class LED13 { 
+public:
+	LED13();
+	~LED13();
+  void on();
+	void off();
+	void blink(int time);
+};
+
+#endif
