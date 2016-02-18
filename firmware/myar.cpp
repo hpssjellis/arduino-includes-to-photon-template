1936dc9a0e3e6058a2a708db380d84215d121297
Jeremy Ellis <keyfreemusic@gmail.com>
2016-02-17 22:22:44 -0800

    Update myar.cpp

--- firmware/myar.cpp
+++ firmware/myar.cpp
@@ -6,7 +6,7 @@
 ///#include "LED13.h" //include the declaration for this class
 #include "myar.h"
 
-const byte LED_PIN = D7;   /// 13; //use the LED @ Arduino pin 13
+const int LED_PIN = D7;   /// 13; //use the LED @ Arduino pin 13
 
 //<<constructor>> setup the LED, make pin 13 an OUTPUT
 LED13::LED13(){
